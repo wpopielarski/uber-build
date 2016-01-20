1bcabfb3c463ca2033d27a6d2c9b71af3c65e4a7
Simon Schäfer <mail@antoras.de>
2016-01-20 23:31:05 +0100

    Resolve Scala artifacts through an URL
    `IDE_M2_REPO` is not a file but a URL

--- uber-build.sh
+++ uber-build.sh
@@ -1133,7 +1133,7 @@ function stepScalaRefactoring () {
     $SBT_RUNNER \
       'set publishTo := Some(Resolver.file("Local M2 repo", file("'$LOCAL_M2_REPO'")))' \
       'set scalaVersion := "'$FULL_SCALA_VERSION'"' \
-      'set resolvers += Resolver.file("Scala artifacts", file("'$IDE_M2_REPO'"))' \
+      'set resolvers += "Scala artifacts" at "'$IDE_M2_REPO'"' \
       publish
 
     storeCache ${SCALA_REFACTORING_P2_ID} "$LOCAL_M2_REPO"
